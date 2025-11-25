434f71a0ca06f7dc27f78a5b2cea53e637a21b50
huseynlituran <huseynlituran17@gmail.com>
2025-11-25 08:27:32 +0000

    rectangle

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 class Rectangle:
+# this class represent rectangle and its atributes
     def __init__(self,width, height):
         self.width = width
         self.height = height
