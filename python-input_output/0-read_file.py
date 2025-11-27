914a9af1035486dbeaddfec0aaeff69db3ca0e7e
huseynlituran <huseynlituran17@gmail.com>
2025-11-27 08:18:35 +0000

    reading file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 def write_file(filename="", text=""):
     """Fayla mətn yazır (overwrite) və yazılan simvol sayını qaytarır."""
     with open(filename, 'w', encoding="utf-8") as f:
