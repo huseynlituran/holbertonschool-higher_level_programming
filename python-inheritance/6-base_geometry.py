e65905a1af3fa107986389601156d6b9ce732449
huseynlituran <huseynlituran17@gmail.com>
2025-11-27 07:12:43 +0000

    base geometry

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module defines BaseGeometry class"""
+
+
+class BaseGeometry:
+    """BaseGeometry class with area method"""
+
+    def area(self):
+        """Raise an Exception - area not implemented"""
+        raise Exception("area() is not implemented")
