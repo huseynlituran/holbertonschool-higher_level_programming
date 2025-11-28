d70232ca94fb33ee6fa163b7ab7d7a91b05eee3d
huseynlituran <huseynlituran17@gmail.com>
2025-11-28 16:32:00 +0000

    foreign key

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,7 +1,7 @@
 -- this query show us how to create foreign key
-CREATE DATABASE hbtn_0d_usa;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
-CREATE TABLE cities (
+CREATE TABLE IF NOT EXISTS cities (
 id INT NOT NULL AUTO_INCREMENT,
 state_id INT NOT NULL,
 name VARCHAR(256) NOT NULL,
