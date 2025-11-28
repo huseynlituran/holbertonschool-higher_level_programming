799b9f4ad5df6bce1803fff1b3b61ee3ff895a4d
huseynlituran <huseynlituran17@gmail.com>
2025-11-28 17:03:54 +0000

    inner join

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,5 @@
 -- this query show us how use import
-SELECT tv_shows.title, tv_show_genres_id
+SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 JOIN tv_show_genres ON tv_shows_id = tv_show_genres.show_id
 ORDER BY tv_shows.title , tv_show_genres.genre_id
