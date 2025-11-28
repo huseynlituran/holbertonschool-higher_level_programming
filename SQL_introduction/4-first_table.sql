9b93877c753721e2c453824301ef950f68cac101
huseynlituran <huseynlituran17@gmail.com>
2025-11-28 14:08:59 +0000

    create table

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,5 +1,5 @@
 -- this query help us to create a table with columns
-CREATE TABLE IF NOT EXSIST first_table(
+CREATE TABLE IF NOT EXIST first_table(
 id INT,
-name VHARCHAR(256)
+name VARCHAR(256)
 );
