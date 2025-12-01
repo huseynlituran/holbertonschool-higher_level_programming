f367fa44915b8557c0a09657efb29a9b16027c6a
huseynlituran <huseynlituran17@gmail.com>
2025-12-01 11:39:30 +0000

    htbn status

---
+++ python-network_1/0-hbtn_status.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+A script that fetches https://intranet.hbtn.io/status
+"""
+import urllib.request
+
+
+if __name__ == "__main__":
+    url = 'https://intranet.hbtn.io/status'
+    # Firewall təhlükəsizliyini keçmək üçün xüsusi başlıq əlavə edirik
+    headers = {'cfclearance': 'true'}
+    req = urllib.request.Request(url, headers=headers)
+
+    with urllib.request.urlopen(req) as response:
+        html = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode('utf-8')))
