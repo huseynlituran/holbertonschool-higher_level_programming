e38a73ed6e2de595221d898ea941092d44b1931f
huseynlituran <huseynlituran17@gmail.com>
2025-12-01 14:41:01 +0000

    api server

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -15,30 +15,25 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
         """
         Handles GET requests and serves data based on the path.
         """
-        # Root endpoint
         if self.path == '/':
             self.send_response(200)
             self.send_header('Content-type', 'text/plain')
             self.end_headers()
             self.wfile.write(b"Hello, this is a simple API!")
 
-        # Data endpoint (JSON)
         elif self.path == '/data':
             self.send_response(200)
             self.send_header('Content-type', 'application/json')
             self.end_headers()
             data = {"name": "John", "age": 30, "city": "New York"}
-            # Dictionary-ni JSON stringinə çeviririk və encode edirik
             self.wfile.write(json.dumps(data).encode('utf-8'))
 
-        # Status endpoint
         elif self.path == '/status':
             self.send_response(200)
             self.send_header('Content-type', 'text/plain')
             self.end_headers()
             self.wfile.write(b"OK")
 
-        # Handle 404 Not Found
         else:
             self.send_response(404)
             self.send_header('Content-type', 'text/plain')
@@ -52,5 +47,9 @@ def run(server_class=HTTPServer, handler_class=SimpleHTTPRequestHandler):
     """
     server_address = ('', 8000)
     httpd = server_class(server_address, handler_class)
-    print("Starting server on port 8000...")
+    print("Starting server on port 8000...", flush=True)
     httpd.serve_forever()
+
+
+if __name__ == '__main__':
+    run()
