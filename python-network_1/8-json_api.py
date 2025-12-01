2d300f3de77b4b7d2fe5124d752a926f89c0b4e0
huseynlituran <huseynlituran17@gmail.com>
2025-12-01 12:19:12 +0000

    json api

--- python-network_1/8-json_api.py
+++ python-network_1/8-json_api.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """
 A script that takes in a letter and sends a POST request to
-http://0.0.0.0:5000/search_user with the letter as a parameter.
+ht.0:5000/search_user with the letter as a parameter.
 """
 import requests
 import sys
 
 
 if __name__ == "__main__":
-    # Əgər arqument verilibsə onu götürürük, yoxdursa boş string ""
+    # arqument verilibsə onu götürürük, yoxdursa boş string ""
     q = ""
     if len(sys.argv) > 1:
         q = sys.argv[1]
